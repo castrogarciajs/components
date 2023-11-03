d2df707a7782f84d9221463ec8aed4e073b894d7
castrogarciajs <johancs.mm@gmail.com>
2023-11-03 15:05:31 -0500

    Add button component to theme package

---
+++ packages/theme/src/components/button.ts
@@ -0,0 +1,22 @@
+import { tv } from '../utils/tv'
+
+export const button = tv({
+  base: [
+    'z-0',
+    'group',
+    'relative',
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'box-border',
+    'appearance-none',
+    'outline-none',
+    'select-none',
+    'whitespace-nowrap',
+    'min-w-max',
+    'font-normal',
+    'subpixel-antialiased',
+    'overflow-hidden',
+    'tap-highlight-transparent',
+  ],
+})
--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,2 +1,3 @@
 export * from './colors'
 export * from './plugins'
+export * from './components'
